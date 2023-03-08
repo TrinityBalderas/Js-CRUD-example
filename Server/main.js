d71d279986abdbc0af461e81c7d6d61a1fe4eb84
Trinity Balderas <trinitybalderas02@gmail.com>
2023-03-08 16:09:03 -0600

    moved HTML to root

--- Server/main.js
+++ Server/main.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const FULLNAME = require("./{} db.json")
+const PERSON = require("./{} db.json")
 let globalId = 3
 
 const PORT = 4004;
@@ -13,18 +13,18 @@ app.use(cors());
 app.use(express.static('../index'))
 
 app.get(`/api/name`, (req, res) => {
-    res.status(200).send(FULLNAME)
+    res.status(200).send(PERSON)
 })
 app.get(`/api/name/:id`, (req, res) => {
-    let index = FULLNAME.findIndex(elem => elem.id === +req.params.id)
+    let index = PERSON.findIndex(elem => elem.id === +req.params.id)
     if(index === -1) {
         res.status(404).send("Name doesn't exist")
     }
-    res.status(200).send(FULLNAME[index])
+    res.status(200).send(PERSON[index])
 })
 app.post(`/api/name`,(req, res) => {
     let  name = req.body 
-    if(FULLNAME.some(e => e.name === name)){
+    if(PERSON.some(e => e.name === name)){
         res.status(400).end()
     } else {
         res.status(200).send(createName(name))
@@ -32,7 +32,7 @@ app.post(`/api/name`,(req, res) => {
 })
 app.put(`/api/name/:id`, (req, res) => {
     const name = req.body
-    let index = FULLNAME.findIndex(elem => elem.id === +req.params.id)
+    let index = PERSON.findIndex(elem => elem.id === +req.params.id)
     if(index === -1){
         res.status(404).send("Name doesn exist")
     }
@@ -42,27 +42,27 @@ app.put(`/api/name/:id`, (req, res) => {
     if(!name.firstName || !name.lastName){
         res.status(400).send("Incomplete data")
     }
-    FULLNAME.splice(index, 1)
-    FULLNAME.push(name)
+    PERSON.splice(index, 1)
+    PERSON.push(name)
     res.status(200).send(name)
 })
 app.patch(`/api/name/:id`, (req, res) => {
     const name = req.body
-    let index = FULLNAME.findIndex(elem => elem.id === +req.params.id)
+    let index = PERSON.findIndex(elem => elem.id === +req.params.id)
     if(index === -1){
         res.status(404).send("Name doesn exist")
     }
     if(!name.firstName || !name.firstName){
         res.status(400).send("Incomplete data")
     }
-    FULLNAME.splice(index, 1)
-    FULLNAME.push(name)
+    PERSON.splice(index, 1)
+    PERSON.push(name)
     res.status(200).send(name)
 })
 app.delete(`/api/name/:id`, (req, res) => {
-    let index = FULLNAME.findIndex(elem => elem.id === +req.params.id)
-    FULLNAME.splice(index, 1)
-    res.status(200).send(FULLNAME)
+    let index = PERSON.findIndex(elem => elem.id === +req.params.id)
+    PERSON.splice(index, 1)
+    res.status(200).send(PERSON)
 })
 
 const createName = (name) => {
@@ -71,7 +71,7 @@ const createName = (name) => {
         firstName: name.firstName,
         lastName: name.lastName
     }
-    FULLNAME.push(newName)
+    PERSON.push(newName)
     globalId++
     return newName
 }
