5a32a4c9fd4ef42219081156156eb43a194740a1
Trinity Balderas <trinitybalderas02@gmail.com>
2023-03-21 13:57:11 -0500

    Added an edit

--- Server/main.js
+++ Server/main.js
@@ -15,6 +15,7 @@ app.use(express.static('../index'))
 app.get(`/api/name`, (req, res) => {
     res.status(200).send(PERSON)
 })
+
 app.get(`/api/name/:id`, (req, res) => {
     let index = PERSON.findIndex(elem => elem.id === +req.params.id)
     if(index === -1) {
@@ -22,43 +23,46 @@ app.get(`/api/name/:id`, (req, res) => {
     }
     res.status(200).send(PERSON[index])
 })
-app.post(`/api/name`,(req, res) => {
+
+app.post('/api/name',(req, res) => {
     let  name = req.body 
-    if(PERSON.some(e => e.name === name)){
-        res.status(400).end()
-    } else {
+        if(!validatePerson(name)){
+            res.status(400).end()
+            return
+        } else {
         res.status(200).send(createName(name))
-    }
+        }
 })
+
 app.put(`/api/name/:id`, (req, res) => {
     const name = req.body
+    console.log("Name: ",name);
     let index = PERSON.findIndex(elem => elem.id === +req.params.id)
-    if(index === -1){
-        res.status(404).send("Name doesn exist")
-    }
-    if(name.id !== +req.params.id) {
-        res.status(400).send("Id mismatch")
-    }
-    if(!name.firstName || !name.lastName){
-        res.status(400).send("Incomplete data")
-    }
+    // if(!validatePerson(name)){
+    //     res.status(400).end()
+    //     return
+    // }
     PERSON.splice(index, 1)
     PERSON.push(name)
+    console.log("Name: ",PERSON[index]);
     res.status(200).send(name)
 })
+
 app.patch(`/api/name/:id`, (req, res) => {
     const name = req.body
     let index = PERSON.findIndex(elem => elem.id === +req.params.id)
     if(index === -1){
         res.status(404).send("Name doesn exist")
     }
-    if(!name.firstName || !name.firstName){
-        res.status(400).send("Incomplete data")
+    if(name.firstName != null){
+        PERSON[index].firstName = name.firstName
     }
-    PERSON.splice(index, 1)
-    PERSON.push(name)
+    if(name.lastName != null){
+        PERSON[index].lastName = name.firstName
+    }
     res.status(200).send(name)
 })
+
 app.delete(`/api/name/:id`, (req, res) => {
     let index = PERSON.findIndex(elem => elem.id === +req.params.id)
     PERSON.splice(index, 1)
@@ -75,6 +79,15 @@ const createName = (name) => {
     globalId++
     return newName
 }
+
+function validatePerson(name){
+    if(PERSON.some(e => e.name === name)){
+        return false
+    } else {
+        return true
+    }
+}
+
 app.listen(PORT, () => {
     console.log(`we are live....${PORT}`);
 })
